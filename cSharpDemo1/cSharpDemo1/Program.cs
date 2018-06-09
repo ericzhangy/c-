7f64da14a00c837558e2e0f07f10fe7da5c305b4
eric <166269@qq.com>
2018-06-09 12:33:09 +0800

    222

--- cSharpDemo1/cSharpDemo1/Program.cs
+++ cSharpDemo1/cSharpDemo1/Program.cs
@@ -13,7 +13,7 @@ namespace cSharpDemo1
             int num;
             num = 12;//this branch test
             ++num;
-            int a;
+            int a;//
             num++;
             Console.WriteLine(num++);
 
